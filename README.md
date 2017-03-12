5130d1e6ac647d74a57f57c4a29244b0ca3029cd
Abdul Rehman <supermind2002@gmail.com>
2017-03-12 17:49:00 +0500

    TFT initialized

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Qibla-Finder
 Arduino Based Qibla finder app
+
+
+
+#Pin Configuration
+
+// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
+// For the Arduino Uno, Duemilanove, Diecimila, etc.:
+//   D0 connects to digital pin 8  (Notice these are
+//   D1 connects to digital pin 9   NOT in order!)
+//   D2 connects to digital pin 2
+//   D3 connects to digital pin 3
+//   D4 connects to digital pin 4
+//   D5 connects to digital pin 5
+//   D6 connects to digital pin 6
+//   D7 connects to digital pin 7
+// For the Arduino Mega, use digital pins 22 through 29
+// (on the 2-row header at the end of the board).
+
+
+// Assign human-readable names to some common 16-bit color values:
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+
+// The control pins for the LCD can be assigned to any digital or
+// analog pins...but we'll use the analog pins as this allows us to
+// double up the pins with the touch screen (see the TFT paint example).
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
--- qibla_finder_v01/qibla_finder_v01.ino
+++ qibla_finder_v01/qibla_finder_v01.ino
@@ -6,13 +6,53 @@ Started at: 12-MAR-2017, 5:18PM
 
 */
 
+//===========================================================================
+
+#include <SPFD5408_Adafruit_GFX.h>    // Core graphics library
+#include <SPFD5408_Adafruit_TFTLCD.h> // Hardware-specific library
+#include <SPFD5408_TouchScreen.h>
+
+
+//===========================================================================
+
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+
+
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+//===========================================================================
 
 void setup() {
-  // put your setup code here, to run once:
+ Serial.begin(9600);   
+  tft.reset();  
+  uint16_t identifier = tft.readID();
+  tft.begin(identifier);  
+  tft.setRotation(0); 
 
-}
+  
+
+}//setup ends here
+//===========================================================================
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-}
+
+  
+
+}//loop Ends here
+//===========================================================================
