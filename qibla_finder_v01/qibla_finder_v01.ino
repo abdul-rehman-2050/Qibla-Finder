03144d786d67c8f4b97d651aaa0d3e806c57de2d
Abdul Rehman <supermind2002@gmail.com>
2017-03-12 17:21:06 +0500

    created hello world

---
+++ qibla_finder_v01/qibla_finder_v01.ino
@@ -0,0 +1,18 @@
+/*
+Qibla Finding App- 
+V0.1
+Arduino Mega
+Started at: 12-MAR-2017, 5:18PM
+
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
